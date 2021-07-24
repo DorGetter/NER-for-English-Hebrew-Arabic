95ba25c5aa5c2607d5843235c7deccf2c6bdb491
liad <liadbenmoshe10@gmail.com>
2021-07-24 17:18:01 +0300

    the result at the same page

--- ner_web.py
+++ ner_web.py
@@ -1,16 +1,19 @@
-from flask import Flask ,request, render_template 
-
+from flask import Flask ,request, render_template ,url_for,redirect
 app = Flask(__name__)
 
-
+data= "ner"
 
 @app.route('/')
 def start_form():
         return render_template("start.html")
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST','GET'])
 def second_form():
-    text=request.form['fname']
-    print(text)
-    return render_template('answer.html',dataToRender=text)
-
+    if request.method=='POST':
+        data=request.form['fname']
+        ##here we call the mode and chang the data
+        ##data=model(data)
+        return render_template('start.html',data=data)
+    else:
+        
+        return render_template('start.html',data=data)
